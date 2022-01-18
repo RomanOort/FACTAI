a04e137af953570707613ec2e6cf96c04cdd1090
thomasvanorden <thomasvanorden@live.nl>
2022-01-18 16:09:01 +0100

    rsync user bug fix

--- packages/sync.sh
+++ packages/sync.sh
@@ -1,2 +1,2 @@
 rsync * tvorden@WS3:/storage2/tvorden/FACT/packages -avzP --exclude "gcn_interpretation/gnn-model-explainer/wandb"
-rsync * tvorden@WS9:/storage/tvorden/FACT/packages -avzP --exclude "gcn_interpretation/gnn-model-explainer/wandb"
+rsync * robolab@WS9:/storage/tvorden/FACT/packages -avzP --exclude "gcn_interpretation/gnn-model-explainer/wandb"
