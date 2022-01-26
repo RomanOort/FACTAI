bc5ec7b08865a85223188e97aae8477ffafe5745
thomasvanorden <thomasvanorden@live.nl>
2022-01-26 17:25:09 +0100

    Bash script uncomments

--- packages/train_rcexplainernoldb_mutagenicity_data_sparsity_1_0.sh
+++ packages/train_rcexplainernoldb_mutagenicity_data_sparsity_1_0.sh
@@ -1,19 +1,19 @@
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=42 &
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=42 &
 wait
 python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=15 &
 wait
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=10 &
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=10 &
 wait
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=3 &
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=3 &
 wait
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=0 &
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=0 &
 wait
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=1 &
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=1 &
 wait
 python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=5 &
 wait
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=8 &
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=8 &
 wait
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=69 &
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=69 &
 wait
-#python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=101
+python explainer_main.py --bmname Mutagenicity --graph-mode --num-gc-layers 3 --explainer-method rcexp_noldb --gpu --ckptdir "./ckpt/Mutagenicity" --multigraph-class 0  --prefix "rcexp_noldb_mutag" --lr 0.001 --bloss-version "sigmoid" --train-data-sparsity 1.0 --epochs 601 --seed=101
