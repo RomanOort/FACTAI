07a44b3d0cf5ac20f756965f7bf5bdc3cd9d3463
thomas <thomas.wiggers@student.uva.nl>
2022-01-31 15:31:43 +0100

    auto plot AUC scores

--- packages/gcn_interpretation/gnn-model-explainer/explainer/test_AUC_method.py
+++ packages/gcn_interpretation/gnn-model-explainer/explainer/test_AUC_method.py
@@ -83,23 +83,83 @@ def include_fn(pred, gt):
 
     return roc_auc_score(reals, preds)
 
-print("============= Ground truth:")
-print(gt)
+noise_range = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
 
-print("============= Prediction:")
-print(pred)
+Original= np.array([0.9908604110735414, 0.9511666705401967, 0.8969179466951889, 0.8475323165197972, 0.8051372667666594, 0.7621745534807047, 0.7367770322271583])
 
-print()
-print("=== END")
-print()
 
-score = original(pred, gt)
-print("original", score)
-print()
+FN= np.array([0.9908604110735414, 0.950301344238759, 0.8941436371803021, 0.8429060484165232, 0.799834142848, 0.7559895125263308, 0.7301814299735965])
 
-score = full_AUC(pred, gt)
-print("full", score)
-print()
 
-score = include_fn(pred, gt)
-print("include FN", score)
+Full = np.array([0.9999730228798774, 0.9993951978190219, 0.9981218106302389, 0.9967593888193426, 0.996027002366392, 0.9950794348599054, 0.9945387911727037])
+
+diff = (1- (FN / Original)) * 100
+
+
+
+
+
+def fts(x):
+    return f"{x:.4f}"
+
+
+print('l'*(len(Original)+1))
+
+print(" \\toprule")
+print("Noise level & ", " & ".join(map(str, noise_range)), "\\\\")
+print(" \\midrule")
+
+print("All (FN+FP+TN+TP) &", " & ".join(map(fts, Full)), "\\\\")
+print("Original &", " & ".join(map(fts, Original)), "\\\\")
+print("False negatives &", " & ".join(map(fts, FN)), "\\\\")
+
+def fts_pct(x):
+    return f"{x:.3f}\%"
+print("full and FN diff &", "& ".join(map(fts_pct, diff)), "\\\\")
+print(" \\bottomrule")
+
+
+
+print("---------------- PG Explainer")
+
+# PG Explainer
+Original= np.array([0.9279181609268776, 0.8809680598039797, 0.8293182050575028, 0.784613966051129, 0.7486899651888039, 0.7178781594272209, 0.6941494930746562])
+FN =      np.array([0.9279181609268776, 0.8799844776438122, 0.8265048443189288, 0.780857252912548, 0.7425332661728661, 0.7105413906910457, 0.6863249416229291])
+Full =    np.array([0.9996371917867928, 0.9988296918249906, 0.9974007349935546, 0.9964389589668325, 0.9944888531005899, 0.9932832678351772, 0.9925871054285493])
+diff = (1- (FN / Original)) * 100
+
+print(" \\toprule")
+print("Noise level & ", " & ".join(map(str, noise_range)), "\\\\")
+print(" \\midrule")
+
+print("All (FN+FP+TN+TP) &", " & ".join(map(fts, Full)), "\\\\")
+print("Original &", " & ".join(map(fts, Original)), "\\\\")
+print("False negatives &", " & ".join(map(fts, FN)), "\\\\")
+
+def fts_pct(x):
+    return f"{x:.3f}\%"
+print("Original and false negatives difference &", "& ".join(map(fts_pct, diff)), "\\\\")
+print(" \\bottomrule")
+
+
+if __name__ == '__main__' and False:
+    print("============= Ground truth:")
+    print(gt)
+
+    print("============= Prediction:")
+    print(pred)
+
+    print()
+    print("=== END")
+    print()
+
+    score = original(pred, gt)
+    print("original", score)
+    print()
+
+    score = full_AUC(pred, gt)
+    print("full", score)
+    print()
+
+    score = include_fn(pred, gt)
+    print("include FN", score)
